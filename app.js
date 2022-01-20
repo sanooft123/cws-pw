6e57eb2f87b018548133bc0a9a32b0fa2b1ad1c2
MOHAMMED SANOOF T <85780106+sanooft123@users.noreply.github.com>
2022-01-20 23:22:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,153 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const Blog = require('./model/blogs.js')
+
+const port = 3000;
+
+const app = express();
+
+//MAKING SERVER//server setup
+
+app.set('view engine', 'ejs');
+
+
+//for connecting the database in mongodb
+const dbURI = 'mongodb+srv://app:apptest123@cluster0.aztj2.mongodb.net/webed?retryWrites=true&w=majority';
+
+
+//mongoose is used 
+//here assync function : function that is used separately and doesnot effect the main function
+mongoose.connect(dbURI)
+    .then(result => {
+
+        app.listen(port);
+        console.log('server running at ', port);
+
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+
+
+
+
+//server setup middleware
+//to get server detials ....200=ok.....304=no changes happened
+app.use(morgan('dev'));
+
+//to make folder public in the site
+app.use(express.static( __dirname + "/public"));
+
+//url parse or decode the entered data
+app.use(express.urlencoded({extended:true}))
+
+
+//home page
+
+
+
+
+app.get('/', (req, res) => {
+
+
+    Blog.find().sort({createdAt : -1})
+    .then(result =>{
+
+        res.render('home', { title: 'home', blogs: result });
+
+
+    })
+    .catch(err => console.log(err))
+
+
+    
+});
+
+//blog page
+app.get('/blog/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    Blog.findById(id)
+        .then(result =>{
+
+            console.log(result);
+
+            res.render('blog', {title:'blog', blogs:result});
+        })
+        .catch(err => console.log(err));
+
+});
+
+//create blog 
+app.get('/create', (req, res) => {
+    res.render('create', { title: 'create blog' });
+});
+
+//user input detials fetching 
+app.post('/create' , (req,res) =>{
+    console.log(req.body);
+
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then(result =>{
+            res.redirect('/')
+        })
+        .catch(err => console.log(err))
+})
+
+
+
+
+
+//editor page
+app.get('/editor', (req, res) => {
+
+
+    Blog.find().sort({createdAt : -1})
+        .then(result =>{
+
+
+            res.render('editor', { title: 'editor', blogs: result });
+
+        })
+        .catch(err => console.log(err));
+
+
+
+});
+
+
+//test create blog
+app.get('/createnew',(req,res) =>{
+
+
+    let firstblog = {
+        heading:'sanoof',
+        body:'my name is mohammed sanoof t . i live in calicut',
+        author:'me',
+    }
+
+    const blog = new Blog(firstblog);
+
+    blog.save()
+        .then(result =>{
+            res.send(result);
+        })
+        .catch(err => console.log(err))
+
+
+})
+
+
+
+
+//middleware
+app.use((req, res) => {
+    res.send('404 not fount , oops!!!');
+});
